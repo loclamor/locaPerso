21f7dd629ec5a015e9a8c7f620a157e57c078a4c
Guillaume <guillaume.jandin@gmail.com>
2013-04-02 23:57:00 +0200

    ajout/modif test services

--- src/test/java/iaws/localisation/services/impl/TestLocalisationService.java
+++ src/test/java/iaws/localisation/services/impl/TestLocalisationService.java
@@ -1,5 +1,38 @@
 package iaws.localisation.services.impl;
 
-public class TestLocalisationService {
+import iaws.localisation.domain.Utilisateur;
 
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestLocalisationService extends TestCase {
+
+	private LocalisationServiceImpl ls;
+
+	public TestLocalisationService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static Test suite()
+    {
+        return new TestSuite(TestLocalisationService.class);
+    }
+
+	public void setUp() throws Exception {
+		 super.setUp();
+	 }
+	
+	 public void tearDown() throws Exception {
+		 super.tearDown();
+	 }
+	 
+	 public void testListerVoisins() {
+		 ls = new LocalisationServiceImpl();
+		 List<Utilisateur> users = ls.listerVoisins("garrett.W@univ-tlse3.fr", 5);
+		 assertNull(users);
+	 }
 }
