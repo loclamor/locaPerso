27b1ea6bdaa65413411f8c51c55511bd39ed903d
Guillaume <guillaume.jandin@gmail.com>
2013-04-02 22:32:49 +0200

    ajout fct localisationService

--- src/main/java/iaws/localisation/services/impl/LocalisationServiceImpl.java
+++ src/main/java/iaws/localisation/services/impl/LocalisationServiceImpl.java
@@ -1,15 +1,32 @@
 package iaws.localisation.services.impl;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import iaws.localisation.data.UtilisateursList;
 import iaws.localisation.domain.Utilisateur;
 import iaws.localisation.services.LocalisationService;
 
 public class LocalisationServiceImpl implements LocalisationService{
 
 	@Override
-	public List<Utilisateur> listerVoisins(String utilisateur, double rayon) {
-		// TODO Auto-generated method stub
+	public List<Utilisateur> listerVoisins(String email, double rayon) {
+		if (null != email) {
+			List<Utilisateur> listVoisins = new ArrayList<Utilisateur>();
+			Utilisateur ref = UtilisateursList.getInstance().search(email);
+			
+			Iterator<Utilisateur> it = UtilisateursList.getInstance().getListeUtilisateur().iterator();
+			while (it.hasNext()){
+				Utilisateur ut = it.next();
+				if (ut.equals(ref)) {
+					if (ut.getCoordonnees().estDansLaZone(ut.getCoordonnees(), rayon)) {
+						listVoisins.add(ut);
+					}
+				}
+			}
+			return listVoisins;
+		}
 		return null;
 	}
 
