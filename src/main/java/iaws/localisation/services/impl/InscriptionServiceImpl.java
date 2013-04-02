3f329a4ce49987dbdaeb58c804c2f8dc2fdaac1b
loclamor <favier.loic.31@gmail.com>
2013-04-02 22:27:33 +0200

    ajout du necessaire pour supporter openStreetMap dans le service d'inscription

--- src/main/java/iaws/localisation/services/impl/InscriptionServiceImpl.java
+++ src/main/java/iaws/localisation/services/impl/InscriptionServiceImpl.java
@@ -1,26 +1,31 @@
 package iaws.localisation.services.impl;
 
 import iaws.localisation.data.UtilisateursList;
+import iaws.localisation.domain.Coordonnees;
 import iaws.localisation.domain.Utilisateur;
 import iaws.localisation.services.InscriptionService;
 
 public class InscriptionServiceImpl implements InscriptionService {
 
-	@Override
+	
 	public int inscrireUtilisateur(Utilisateur utilisateur) {
-		// TODO Auto-generated method stub
 		
 		//adresse email deja utilisee
 		if (UtilisateursList.getInstance().search(utilisateur.getAdresseEmail()) == null)
 			return 100;
+		
 		//adresse email invalide
 		if (!utilisateur.getAdresseEmail().endsWith("@univ-tlse3.fr") || !utilisateur.isEmailAdress())
 			return 110;
+		
 		//adresse postale non connue de open street map
-		if (true)
+		OpenStreetMapServiceImpl osmService = new OpenStreetMapServiceImpl();
+		Coordonnees coords = osmService.getCoordonneesAdresse(utilisateur.getAdressePostale());
+		if (!coords.estValide())
 			return 200;
 		//sauvegarde des informations de l utilisateur et des coordonnees de l adresse postale 
-		
+		utilisateur.setCoordonnees(coords);
+		UtilisateursList.getInstance().ajouter(utilisateur);
 		return 0;
 	}
 
