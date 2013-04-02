4ca8f925bd7548bf2292d2ddc11d59cc574f4bed
Guillaume <guillaume.jandin@gmail.com>
2013-04-02 19:52:41 +0200

    ajout fonction dans localisationService

--- src/main/java/iaws/localisation/services/impl/InscriptionServiceImpl.java
+++ src/main/java/iaws/localisation/services/impl/InscriptionServiceImpl.java
@@ -1,6 +1,7 @@
 package iaws.localisation.services.impl;
 
 import iaws.localisation.domain.Utilisateur;
+import iaws.localisation.domain.UtilisateursList;
 import iaws.localisation.services.InscriptionService;
 
 public class InscriptionServiceImpl implements InscriptionService {
@@ -8,6 +9,18 @@ public class InscriptionServiceImpl implements InscriptionService {
 	@Override
 	public int inscrireUtilisateur(Utilisateur utilisateur) {
 		// TODO Auto-generated method stub
+		
+		//adresse email deja utilisee
+		if (UtilisateursList.getInstance().search(utilisateur.getAdresseEmail()) == null)
+			return 100;
+		//adresse email invalide
+		if (!utilisateur.getAdresseEmail().endsWith("@univ-tlse3.fr") || !utilisateur.isEmailAdress())
+			return 110;
+		//adresse postale non connue de open street map
+		if (true)
+			return 200;
+		//sauvegarde des informations de l utilisateur et des coordonnees de l adresse postale 
+		
 		return 0;
 	}
 
