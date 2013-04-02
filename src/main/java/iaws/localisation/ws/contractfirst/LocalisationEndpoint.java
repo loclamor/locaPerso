e9afe6755202fc52ea8aa379e0f9004226cd8284
loclamor <favier.loic.31@gmail.com>
2013-04-02 12:55:43 +0200

    ajout du endpoint principal vide

---
+++ src/main/java/iaws/localisation/ws/contractfirst/LocalisationEndpoint.java
@@ -0,0 +1,34 @@
+package iaws.localisation.ws.contractfirst;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.*;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+
+//import com.mycompany.hr.service.HumanResourceService;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.Namespace;
+import org.jdom.xpath.XPath;
+
+@Endpoint
+public class LocalisationEndpoint {
+
+	@Autowired
+	public LocalisationEndpoint() throws JDOMException {
+
+	}
+
+	@PayloadRoot(namespace = "http://localisationPersonnel.com/rechercheProche/schema", localPart = "rechercheUtilisateursProchesRequest")
+	public void handleRechercheUtilisateurRequest(@RequestPayload Element rechercheUtilisateursProchesRequest)
+			throws Exception {
+
+	}
+	
+	@PayloadRoot(namespace = "http://localisationPersonnel.com/inscription/schema", localPart = "inscriptionUtilisateurResponse")
+	public void handleInscriptionUtilisateurRequest(@RequestPayload Element inscriptionUtilisateurResponse)
+			throws Exception {
+
+	}
+}
